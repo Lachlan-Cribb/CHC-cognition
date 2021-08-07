446ca9482196691a1bfafc27a1ce79abc279765f
Lachlan-epi <lachycribb@gmail.com>
2021-08-07 23:07:39 +1000

    Transformation of RT variables, imputed missing data

--- Compromise V4.R
+++ Compromise V4.R
@@ -4,12 +4,12 @@ library(lavaan)
 library(psych)
 library(semTools)
 library(semPlot)
+library(mice)
 
 
 ## Load data 
 
-arcli <- read_sav("C:\\Users\\lachy\\Dropbox\\ARCLI data entry\\BLINDED DATA SETS\\BLINDED _DO NOT TOUCH\\ARCLI_V1_Baseline_ALL_18-JUN-2021.sav")
-
+arcli <- read_sav("C:\\Users\\lachy\\Dropbox\\ARCLI data entry\\BLINDED DATA SETS\\BLINDED _DO NOT TOUCH\\ARCLI_V1_Baseline_ALL_13-JUL-2021.sav")
 
 # remove -999
 
@@ -28,19 +28,18 @@ arcli <- arcli %>%
   rename(CDR_DelayedWRCorrect_v1 = `CDR_DelayedWRCorrect#_v1`)
 
 
-# create new overall scores where missing
-
-
-arcli$CDR_PicRecOverallAcc_v1 <- (arcli$CDR_PicRecNovelStimAcc_v1 + arcli$CDR_PicRecOrigStimAcc_v1) / 2
-
-arcli$CDR_Spatial_WM_acc_v1 <- (arcli$CDR_SpatialWMNovelStimAcc_v1 + arcli$CDR_SpatialWMOrigStimAcc_v1) / 2
+# create new overall score
 
 arcli$CDR_NumericWMOverallAcc_v1 <- (arcli$CDR_NumericWMOrigStimAcc_v1 + arcli$CDR_NumericWMNewStimAcc_v1) / 2
 
+# set sex variable to factor
+
+arcli$Sex <- as.factor(arcli$Sex)
+
+levels(arcli$Sex) <- c("male", "female")
 
 
-
-# select cognitive important variables 
+# select relevant variables 
 
 cog_vars <- arcli %>% 
   dplyr::select(Sex, IDno, CDR_RVIPRTms_v1, Initialage,CDR_RVIPacc_v1,TMT_Bms_v1, CDR_ImmWRCorrect_v1, CDR_DelayedWRCorrect_v1, 
@@ -48,23 +47,32 @@ cog_vars <- arcli %>%
                 CDR_DigitVigRTms_v1, SCB_ConStroopRTms_v1, SCB_InconStroopRTms_v1, 
                 Jenson8ChoiceDecisionTimems_v1,Jenson4ChoiceDecisionTimems_v1,
                 Jenson1ChoiceDecisionTimems_v1,WASImatrixreasoningrawscore, 
-                CDR_RVIPacc_v1,WASI_vocabrawscore, MMSE, Serial3sAcc_v1, Serial7sAcc_v1, TMT_Ams_v1)
-
+                WASI_vocabrawscore, MMSE, Serial3sAcc_v1, Serial7sAcc_v1, TMT_Ams_v1)
 
 
 ## Remove rows without cog results
 
-
 cog_vars <- cog_vars[which(rowMeans(is.na(cog_vars)) < 0.80),]
-
 
 # check histograms #
 
 #multi.hist(cog_vars[,-1])
 
 
-# winsorise extreme outliers 
+# log transform RT variables
 
+cont_vars <- cog_vars %>% select(
+  CDR_RVIPRTms_v1,,TMT_Bms_v1,CDR_SimpleRTms_v1, CDR_ChoiceRTms_v1, 
+  CDR_DigitVigRTms_v1, SCB_ConStroopRTms_v1, SCB_InconStroopRTms_v1, 
+  Jenson8ChoiceDecisionTimems_v1,Jenson4ChoiceDecisionTimems_v1,
+  Jenson1ChoiceDecisionTimems_v1, TMT_Ams_v1) %>% 
+  names() # variable names
+
+cog_vars <- cog_vars %>% 
+  mutate(across(all_of(cont_vars), ~ log2(.), .names = "log{.col}")) # log 2 transform
+
+
+# winsorise outliers for RT vars
 
 winsorise <- function(x, sd){
   
@@ -81,35 +89,23 @@ winsorise <- function(x, sd){
   
 }
 
-
 # Winsorise outliers 
 
+cog_vars <- cog_vars %>% # RT measures
+  mutate(across(starts_with("log"), winsorise, sd = 3, .names = "w{.col}"))
 
-cog_vars$CDR_SimpleRTms_v1c <- winsorise(cog_vars$CDR_SimpleRTms_v1, sd = 4)
-
-cog_vars$CDR_ChoiceRTms_v1c <- winsorise(cog_vars$CDR_ChoiceRTms_v1, sd = 4)
-
-cog_vars$Jenson4ChoiceDecisionTimems_v1c <- winsorise(cog_vars$Jenson4ChoiceDecisionTimems_v1, sd = 4)
-
-cog_vars$Jenson1ChoiceDecisionTimems_v1c <- winsorise(cog_vars$Jenson1ChoiceDecisionTimems_v1, sd =4)
-
-cog_vars$TMT_Ams_v1c <- winsorise(cog_vars$TMT_Ams_v1, sd = 4)
-
-cog_vars$WASI_vocabrawscorec <- winsorise(cog_vars$WASI_vocabrawscore, sd = 4)
-
-cog_vars$TMT_Bms_v1c <- winsorise(cog_vars$TMT_Bms_v1, sd = 4)
-
-cog_vars$TMT_Bms_v1c <- winsorise(cog_vars$TMT_Bms_v1c, sd = 4)
-
-
+cog_vars <- cog_vars %>% # WASI measures
+  mutate(across(starts_with("WASI"), winsorise, sd = 3, .names = "w{.col}"))
 
 ## remove cases with more than half cog vars missing
 
-
 cog_vars <- cog_vars[which(rowMeans(is.na(cog_vars)) < 0.50),]
 
-  
+# check missing data per variable
 
+n_miss <- colSums(is.na(cog_vars))
+
+sort(n_miss / 514)
 
 # Reverse score reaction time measures
 
@@ -119,89 +115,69 @@ reverse_RT <- function(x){
   x
 }
 
+cog_vars <- cog_vars %>% 
+  mutate(across(starts_with("wlog"), reverse_RT, .names = "{.col}_rev"))
+
+
+## rescale accuracy variables (10% units)
+
+cog_vars <- cog_vars %>% mutate(across(starts_with("Serial"), ~  . / 10))
+
+cog_vars <- cog_vars %>% mutate(CDR_NumericWMOverallAcc_v1 = CDR_NumericWMOverallAcc_v1 / 10)
+
+# rescale WASI scores to per 10 points 
 
 cog_vars <- cog_vars %>% 
-  mutate_at(vars("CDR_DigitVigRTms_v1","CDR_RVIPRTms_v1", "CDR_SimpleRTms_v1c",
-                 "CDR_ChoiceRTms_v1c","Jenson8ChoiceDecisionTimems_v1",
-                 "Jenson4ChoiceDecisionTimems_v1c", 
-                 "Jenson1ChoiceDecisionTimems_v1c",
-                 "SCB_ConStroopRTms_v1", 
-                 "SCB_InconStroopRTms_v1",
-                 "TMT_Ams_v1c", "TMT_Bms_v1c"),
-            .funs = list(rev = ~ reverse_RT(.)))
-
-
-
-## Rescale indicators 
-
-# RT measures to per 25 miliseconds
-
-cog_vars <- cog_vars %>% 
-  mutate_at(vars("CDR_RVIPRTms_v1_rev","CDR_DigitVigRTms_v1_rev","CDR_SimpleRTms_v1c_rev",
-                 "CDR_ChoiceRTms_v1c_rev","Jenson8ChoiceDecisionTimems_v1_rev",
-                 "Jenson4ChoiceDecisionTimems_v1c_rev", "Jenson1ChoiceDecisionTimems_v1c_rev"),
-            .funs = ~ . / 25)
-
-
-# stroop vars to per 50 miliseconds
-
-
-cog_vars <- cog_vars %>% 
-  mutate_at(vars("SCB_ConStroopRTms_v1_rev", "SCB_InconStroopRTms_v1_rev"),
-            .funs = ~ . / 50)
-
-
-# Serial meausre per 10% 
-
-
-cog_vars <- cog_vars %>% 
-  mutate_at(vars("Serial3sAcc_v1","Serial7sAcc_v1"),
+  mutate_at(vars("wWASImatrixreasoningrawscore",
+                 "wWASI_vocabrawscore"),
             .funs = ~ . / 10)
 
+# select relevant variables #
 
-# Numeric WM acc per 5%
+cog_vars2 <- cog_vars %>% 
+  select(IDno, Sex, Initialage, ends_with("_rev"), CDR_ImmWRCorrect_v1, wWASI_vocabrawscore, wWASImatrixreasoningrawscore,
+         CDR_DelayedWRCorrect_v1, CDR_NumericWMOverallAcc_v1, MMSE,
+         Serial3sAcc_v1, Serial7sAcc_v1)
 
-cog_vars$CDR_NumericWMOverallAcc_v1 <- cog_vars$CDR_NumericWMOverallAcc_v1 / 5
+## single imputation
 
+# predictive mean matching for continuous vars, proportional odds for ordinal
 
-# TMT to 5 seconds 
+predictormatrix <- quickpred(cog_vars2, 
+                           include=c("Sex", "Initialage"),
+                           exclude="IDno",
+                           mincor = 0.1)
 
-cog_vars$TMT_Ams_v1c_rev <- cog_vars$TMT_Ams_v1c_rev / 5000 
+cog_vars2_imp <- mice(cog_vars2, m = 1, method = "pmm",
+                      predictorMatrix = predictormatrix, diagnostics = T)
 
+imp_dat <- complete(cog_vars2_imp) # imputed data 
 
-cog_vars$TMT_Bms_v1c_rev <- cog_vars$TMT_Bms_v1c_rev / 5000 
+# check imputation for each variable 
 
+mice::stripplot(cog_vars2_imp, wlogCDR_RVIPRTms_v1_rev + wlogTMT_Bms_v1_rev ~ .imp)
 
-# WASI scores to per 2.5 points 
-
-cog_vars <- cog_vars %>% 
-  mutate_at(vars("WASImatrixreasoningrawscore",
-                 "WASI_vocabrawscore"),
-            .funs = ~ . / 2.5)
-
-
-
+varTable(imp_dat)
 
 # define CFA model 
-
 
 model <- 
   
 ' # model 
 
-Gt =~  CDR_SimpleRTms_v1c_rev +
-       CDR_ChoiceRTms_v1c_rev +
-       CDR_DigitVigRTms_v1_rev +
-       Jenson8ChoiceDecisionTimems_v1_rev +
-       Jenson4ChoiceDecisionTimems_v1c_rev +
-       Jenson1ChoiceDecisionTimems_v1c_rev
+Gt =~  wlogCDR_SimpleRTms_v1_rev +
+       wlogCDR_ChoiceRTms_v1_rev +
+       wlogCDR_DigitVigRTms_v1_rev +
+       wlogJenson8ChoiceDecisionTimems_v1_rev +
+       wlogJenson4ChoiceDecisionTimems_v1_rev +
+       wlogJenson1ChoiceDecisionTimems_v1_rev
        
-Gs =~  SCB_ConStroopRTms_v1_rev +
-       SCB_InconStroopRTms_v1_rev +
-       TMT_Ams_v1c_rev +
+Gs =~  wlogSCB_ConStroopRTms_v1_rev +
+       wlogSCB_InconStroopRTms_v1_rev +
+       wlogTMT_Ams_v1_rev +
        # cross loading
-       TMT_Bms_v1c_rev +
-       CDR_ChoiceRTms_v1c_rev 
+       wlogTMT_Bms_v1_rev +
+       wlogCDR_ChoiceRTms_v1_rev 
 
        
 Glr_m6 =~ CDR_ImmWRCorrect_v1 +
@@ -211,34 +187,30 @@ Glr_m6 =~ CDR_ImmWRCorrect_v1 +
 Gwm_ac =~ Serial3sAcc_v1 +
         Serial7sAcc_v1 +
         CDR_NumericWMOverallAcc_v1 +
-        TMT_Bms_v1c_rev
+        wlogTMT_Bms_v1_rev
 
-Gf =~ WASImatrixreasoningrawscore
+Gf =~ wWASImatrixreasoningrawscore
 
 Gc =~ MMSE +
-      WASI_vocabrawscorec
+      wWASI_vocabrawscore
 
 
 # correlated residuals
-TMT_Ams_v1c_rev ~~ TMT_Bms_v1c_rev
+wlogTMT_Ams_v1_rev ~~ wlogTMT_Bms_v1_rev
 Serial3sAcc_v1 ~~ Serial7sAcc_v1
-Jenson4ChoiceDecisionTimems_v1c_rev ~~ Jenson1ChoiceDecisionTimems_v1c_rev
-Jenson8ChoiceDecisionTimems_v1_rev ~~ Jenson1ChoiceDecisionTimems_v1c_rev
-Jenson8ChoiceDecisionTimems_v1_rev ~~ Jenson4ChoiceDecisionTimems_v1c_rev
-WASImatrixreasoningrawscore ~~ 0.10*WASImatrixreasoningrawscore
+wlogJenson4ChoiceDecisionTimems_v1_rev ~~ wlogJenson1ChoiceDecisionTimems_v1_rev
+wlogJenson8ChoiceDecisionTimems_v1_rev ~~ wlogJenson1ChoiceDecisionTimems_v1_rev
+wlogJenson8ChoiceDecisionTimems_v1_rev ~~ wlogJenson4ChoiceDecisionTimems_v1_rev
 '
-
 
 # fit model
 
-test1 <- cfa(model, data = cog_vars, missing = "fiml", 
-             estimator = "MLR", std.lv = F)
+test1 <- cfa(model, data = imp_dat, estimator = "MLR", std.lv = F)
 
 
 # results
 
 summary(test1, fit.measures=T, standardized = T)
-
 
 
 ## SEM plot
@@ -249,25 +221,24 @@ semPaths(test1, intercepts = F, residuals = F,
          filename = "sem plot CHC", width = 25,
          reorder = F, latents = c("Gt","Gs", "Gwm_ac","Gl_m6","Gf","Gc"))
 
-?semPaths
 
 ## Save factor scores ##
 
-
-preds <- as_tibble(lavPredict(test1, newdata = cog_vars, type = "lv",
+preds <- as_tibble(lavPredict(test1, newdata = imp_dat, type = "lv",
                     method = "Bartlett"))
 
 
-new <- cbind(cog_vars, preds)
+new <- cbind(imp_dat, preds)
 
 ## save only factor scores and IDvar to merge back into data
 
 new <- new %>% select(IDno, Gt:Gc)
 
-
 ## merge factor scores into original data
 
 arcli2 <- arcli %>% full_join(new, by = "IDno")
+
+write_sav(arcli2, "arcli with CHC.sav")
 
 
 
